48dbb1a445d6a2b7a972a7b4939be59dc49afc04
Soumya Ranjan Rout <soumyarout80@gmail.com>
2021-04-12 01:36:21 +0530

    Added region for Frankfurt

--- aws-module/outputs.tf
+++ aws-module/outputs.tf
@@ -3,7 +3,4 @@ output "id" {
   description = "The ID of the VPC."
 }
 
-//output "tags" {
-//  value       = module.vpc.tags
-//  description = "A mapping of tags to assign to the resource."
-//}
+
