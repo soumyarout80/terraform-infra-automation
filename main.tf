03a9b4758873c9aebd8e22d92b46761a1ff7b023
Soumya Ranjan Rout <soumyarout80@gmail.com>
2021-04-12 02:59:12 +0530

    Added SG for SSH 22 port

--- main.tf
+++ main.tf
@@ -197,6 +197,13 @@ resource "aws_security_group" "allow_http" {
       "0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
