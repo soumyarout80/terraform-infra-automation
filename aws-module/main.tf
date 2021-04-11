f24bbacc9276f7b69db6af6aff3b6e345f42ee38
Soumya Ranjan Rout <soumyarout80@gmail.com>
2021-04-12 01:35:36 +0530

    Added region for Frankfurt

--- aws-module/main.tf
+++ aws-module/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   label_order = ["name", "environment"]
   cidr_block = "20.10.0.0/16" # 10.0.0.0/8 is reserved for EC2-Classic
 
-  azs                 = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs                 = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   private_subnets     = ["20.10.1.0/24", "20.10.2.0/24", "20.10.3.0/24"]
   public_subnets      = "20.10.11.0/24"
   database_subnets    = ["20.10.21.0/24", "20.10.22.0/24", "20.10.23.0/24"]
