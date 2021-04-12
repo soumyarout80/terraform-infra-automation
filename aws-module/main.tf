c19a8f8162899fe1ff6c266e6e16d19bb22bf397
Soumya Ranjan Rout <soumyarout80@gmail.com>
2021-04-12 20:29:46 +0530

    Modified VPC module

--- aws-module/main.tf
+++ aws-module/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
 
   azs                 = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   private_subnets     = ["20.10.1.0/24", "20.10.2.0/24", "20.10.3.0/24"]
-  public_subnets      = "20.10.11.0/24"
+  public_subnets      = ["20.10.11.0/24", "20.10.12.0/24", "20.10.13.0/24"]
   database_subnets    = ["20.10.21.0/24", "20.10.22.0/24", "20.10.23.0/24"]
 
   vpc_enabled         = true
