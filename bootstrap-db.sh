79c98dd5f7026a988d6e018c1328fe21829ea586
Soumya Ranjan Rout <soumyarout80@gmail.com>
2021-04-12 20:27:43 +0530

    Added boot startp script for mongodb

---
+++ bootstrap-db.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+##### Install Ansible ######
+apt-get update
+apt-get install -y git wget ansible
+
+##### Clone your ansible repository ######
+git clone https://github.com/soumyarout80/terraform-infra-automation.git
+cd terraform-infra-automation/ansible
+
+##### Run your ansible playbook for only autoscaled and not initialised instances ######
+ansible-playbook --connection=local --inventory 127.0.0.1, nginx-web-server.yml
